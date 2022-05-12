3016385d28093fd197ce67666aec5e8648c1a4c6
Muhammad-21 <muhammadmahmudov21@yandex.ru>
2022-05-12 21:39:18 +0300

    added Confeti

--- src/pages/Pay.js
+++ src/pages/Pay.js
@@ -1,19 +1,30 @@
 import React, { useEffect } from 'react'
+import Confetti from 'react-confetti';
 import {useNavigate } from 'react-router-dom'
+import { useWindowSize } from 'react-use';
 
 export default function Pay() {
     const navigate = useNavigate();
+    const { width, height } = useWindowSize()
     useEffect(() => {
         setTimeout(() => {
           navigate('/')
         }, 3000)
       }, [])
   return (
+    <div>
     <div className="pay">
         <div>
             <h2 style={{color:"#55e720"}}>Покупка прошла успешно ⁣ &#128521;</h2>
             <p style={{marginLeft:"10px"}}>перенаправляю на главную страничку ...</p>
         </div>
+    </div>
+    <Confetti
+              width={width}
+              height={height}
+              colors={['#55e720','orange']}
+              numberOfPieces={300}
+            />
     </div>
   )
 }
